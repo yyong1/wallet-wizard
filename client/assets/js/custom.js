cf8f75a655fd7463c471463d6c4c009e84ba6838
Andrey Baranov <baranov.a.64103@gmail.com>
2023-06-11 23:48:49 +0200

    spapp fix

--- client/assets/js/custom.js
+++ client/assets/js/custom.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
 
   $("main#spapp > section").height($(document).height() - 60);
 
-  var app = $.spapp({ pageNotFound: 'error_404' }); // initialize
+  var app = $.spapp({
+    pageNotFound: 'error_404'
+  }); // initialize
 
   // define routes
   app.route({
@@ -29,19 +31,6 @@ $(document).ready(function () {
     view: 'accounts',
     load: 'accounts.html'
   });
-
-  // Example:
-
-  // app.route({
-  //   view: 'view_1',
-  //   onCreate: function() { $("#view_1").append($.now()+': Written on create<br/>'); },
-  //   onReady: function() { $("#view_1").append($.now()+': Written when ready<br/>'); }
-  // });
-  // app.route({view: 'view_2', load: 'view_2.html' });
-  // app.route({
-  //   view: 'view_3', 
-  //   onCreate: function() { $("#view_3").append("I'm the third view"); }
-  // });
 
   // run app
   app.run();
