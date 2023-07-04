8c4857e31f0af9bfa43d125686b95518f77abe04
Andrey Baranov <baranov.a.64103@gmail.com>
2023-07-04 02:35:32 +0200

    probably working exp func

--- assets/js/expenses.js
+++ assets/js/expenses.js
@@ -1,35 +1,54 @@
 var expenses = {
     getexpenses: function () {
-        var id = 5;
-        $.get('rest/expenses/'+id, function (data) {
+        var token = localStorage.getItem('jwt_token');
+        var decodedToken = jwt_decode(token); // !!!!!!!!!!!!!!!
+        var id = decodedToken.UserId;
+
+
+        $.get('rest/expenses/' + id, function (data) {
             console.log("Size of data:", data.length);
             var expensesHtml = "";
             //this is my new branch expenses
-            var sum=0;
-            for(var i=0; i<data.length; i++){
+            var sum = 0;
+
+            for (var i = 0; i < data.length; i++) {
                 var expense = data[i];
-                sum+=parseFloat(expense.Amount);
+                sum += parseFloat(expense.Amount);
                 var list = "";
-                list = 
-                <tr>
-                    <th scope="row">${expense.CategoryName}</th>
-                    <td>${expense.SubCategoryName}</td>
-                    <td>${expense.Amount}$</td>
-                </tr>
+                list =
+                    // <tr>
+                    //     <th scope="row">${expense.CategoryName}</th>
+                    //     <td>${expense.SubCategoryName}</td>
+                    //     <td>${expense.Amount}$</td>
+                    // </tr>
+
+                `<div class="media text-muted pt-3">
+                    <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect fill="#007bff" width="100%" height="100%"></rect><text fill="#007bff" dy=".3em" x="50%" y="50%">32x32</text></svg>
+                    <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                        <div class="d-flex justify-content-between align-items-center w-100">
+                            <strong class="text-gray-dark">${expense.CategoryName}</strong>
+                            <strong class="text-gray-dark">${expense.Amount}</strong>
+                
+                            {/* <a href="#">Follow</a> */}
+                        </div>
+                        <span class="d-block">${expense.SubCategoryName}</span>
+                    </div>
+                </div>`;
+
                 expensesHtml += list;
             }
-            sum=sum.toString();
-            var totalRow = "";
-                totalRow = 
-                <tr>
-                    <th scope="row">---</th>
-                    <td>TOTAL AMOUNT</td>
-                    <td>${sum}$</td>
-                </tr>
-            expensesHtml += totalRow;
+            // sum = sum.toString();
+            // var totalRow = "";
+            // totalRow =
+            //     <tr>
+            //         <th scope="row">---</th>
+            //         <td>TOTAL AMOUNT</td>
+            //         <td>${sum}$</td>
+            //     </tr>
+            // expensesHtml += totalRow;
 
-
-            $("#expenses-list").html(expensesHtml);
+            $(".card-for-data").html(expensesHtml);
+            // $("#expenses-list").html(expensesHtml);
         });
     }
 
--- assets/js/income.js
+++ assets/js/income.js
@@ -1,26 +1,28 @@
 var income = {
     getincome: function () {
-        var id = 5;
-        $.get('rest/incomes/'+id, function (data) {
+        var token = localStorage.getItem('jwt_token');
+        var decodedToken = jwt_decode(token);
+        var id = decodedToken.UserId;
+        $.get('rest/incomes/' + id, function (data) {
             console.log("Size of data:", data.length);
             var incomeHtml = "";
             //this is my new branch expenses
-            var sum=0;
-            for(var i=0; i<data.length; i++){
+            var sum = 0;
+            for (var i = 0; i < data.length; i++) {
                 var income = data[i];
-                sum+=parseFloat(income.Amount);
+                sum += parseFloat(income.Amount);
                 var list = "";
-                list = 
-                <tr>
-                    <th scope="row">${income.CategoryName}</th>
-                    <td>${income.SubCategoryName}</td>
-                    <td>${income.Amount}$</td>
-                </tr>
+                list =
+                    <tr>
+                        <th scope="row">${income.CategoryName}</th>
+                        <td>${income.SubCategoryName}</td>
+                        <td>${income.Amount}$</td>
+                    </tr>
                 incomeHtml += list;
             }
-            sum=sum.toString();
+            sum = sum.toString();
             var totalRow = "";
-                totalRow = 
+            totalRow =
                 <tr>
                     <th scope="row">---</th>
                     <td>TOTAL AMOUNT</td>
