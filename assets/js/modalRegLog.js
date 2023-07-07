08ee9d4f1ab5f607143fd7170ff69c950f64926d
Andrey Baranov <baranov.a.64103@gmail.com>
2023-07-08 00:03:52 +0200

    modal hide fix

--- assets/js/modalRegLog.js
+++ assets/js/modalRegLog.js
@@ -1,46 +1,46 @@
 var isSignUp = true;
 
 var modalElement = `
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">${isSignUp ? 'Registration' : 'Login'}</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <form id="login-form">
-            ${isSignUp ? `
-            <div class="form-group">
-              <label for="user-name" class="col-form-label">Name:</label>
-              <input type="text" class="form-control" id="user-name" required>
-            </div>
-            ` : ''}
-            <div class="form-group">
-              <label for="email-name" class="col-form-label">Email:</label>
-              <input type="email" class="form-control" id="email-name" required>
-            </div>
-            <div class="form-group">
-              <label for="password-name" class="col-form-label">Password:</label>
-              <input type="password" class="form-control" id="password-name" required minlength="8" maxlength="16">
-            </div>
-            ${isSignUp ? `
-            <div class="form-group">
-              <label for="repeat-password-name" class="col-form-label">Repeat password:</label>
-              <input type="password" class="form-control" id="repeat-password-name" required>
-            </div>
-            ` : ''}
-          </form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary close-footer" data-dismiss="modal">Close</button>
-          <button type="submit" form="login-form" class="btn btn-primary btn-action">${isSignUp ? `Register` : `Login`}</button>
-        </div>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${isSignUp ? 'Registration' : 'Login'}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          ${isSignUp ? `
+          <div class="form-group">
+            <label for="user-name" class="col-form-label">Name:</label>
+            <input type="text" class="form-control" id="user-name">
+          </div>
+          ` : ''}
+          <div class="form-group">
+            <label for="email-name" class="col-form-label">Email:</label>
+            <input type="text" class="form-control" id="email-name">
+          </div>
+          <div class="form-group">
+            <label for="password-name" class="col-form-label">Password:</label>
+            <input type="password" class="form-control" id="password-name">
+          </div>
+          ${isSignUp ? `
+          <div class="form-group">
+            <label for="repeat-password-name" class="col-form-label">Repeat password:</label>
+            <input type="password" class="form-control" id="repeat-password-name">
+          </div>
+          ` : ''}
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary close-footer" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary btn-action">${isSignUp ? `Register` : `Login`}</button>
       </div>
     </div>
   </div>
+</div>
 `;
 
 $("body").append(modalElement);
@@ -75,39 +75,6 @@ $("body").on("click", ".btn-header-login", function () {
 
 $("body").on("click", ".close, .close-footer", function () {
   $("#exampleModal").modal("hide");
-});
-
-$("#login-form").validate({
-  rules: {
-    email: {
-      required: true,
-      email: true
-    },
-    password: {
-      required: true,
-      minlength: 8,
-      maxlength: 16
-    },
-    repeat_password: {
-      required: isSignUp,
-      equalTo: "#password-name"
-    }
-  },
-  messages: {
-    email: "Enter a valid email address",
-    password: {
-      required: "Enter a password",
-      minlength: "Password must be at least 8 characters long",
-      maxlength: "Password must not exceed 16 characters"
-    },
-    repeat_password: {
-      required: "Repeat your password",
-      equalTo: "Passwords do not match"
-    }
-  },
-  submitHandler: function(form) {
-    performAction();
-  }
 });
 
 function performAction() {
@@ -150,12 +117,17 @@ function registerUser(name, email, password) {
       localStorage.setItem("jwt_token", token);
       updateNavbar();
       window.location.href = "#income";
-
+      $("#exampleModal").modal("hide"); 
+      toastr.success("Reg");
       console.log(response, "success register user");
     },
     error: function (xhr, status, error) {
-      console.log("reg fail");
-      console.error(error);
+      if (xhr.status === 401) {
+        toastr.error("Validation failed: Invalid input");
+      } else {
+        console.log("reg fail");
+        console.error(error);
+      }
     }
   });
 }
@@ -180,12 +152,17 @@ function loginUser(email, password) {
       localStorage.setItem("jwt_token", token);
       updateNavbar();
       window.location.href = "#income";
-
+      $("#exampleModal").modal("hide");
+      toastr.success("Reg");
       console.log(response, "success login user");
     },
     error: function (xhr, status, error) {
-      console.log("login fail");
-      console.error(error);
+      if (xhr.status === 401) {
+        toastr.error("Validation failed: Invalid input");
+      } else {
+        console.log("login fail");
+        console.error(error);
+      }
     }
   });
 }
