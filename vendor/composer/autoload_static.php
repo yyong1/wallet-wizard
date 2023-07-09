beefceddd175b9886bc69964f5eab22a7a818873
Emir0Bekrija <emir.bekrija@stu.ibu.edu.ba>
2023-07-09 02:59:02 +0200

    lollolol

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -100,9 +100,9 @@ class ComposerStaticInit2f16677b28778ea12954b78ed1fd15d0
         ),
         'Illuminate\\Support\\' => 
         array (
-            0 => __DIR__ . '/..' . '/illuminate/macroable',
+            0 => __DIR__ . '/..' . '/illuminate/collections',
             1 => __DIR__ . '/..' . '/illuminate/conditionable',
-            2 => __DIR__ . '/..' . '/illuminate/collections',
+            2 => __DIR__ . '/..' . '/illuminate/macroable',
             3 => __DIR__ . '/..' . '/illuminate/support',
         ),
         'Illuminate\\Hashing\\' => 
