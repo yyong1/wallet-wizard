19c8011546d7ac9c049af7c691ecc6dcc65c2da9
Andrey Baranov <baranov.a.64103@gmail.com>
2023-07-09 02:37:51 +0200

    front ajax prepared

--- assets/js/addModal.js
+++ assets/js/addModal.js
@@ -1,4 +1,11 @@
 // Modal for adding new expense
+
+
+function getCategoryToSelect() {
+    // get current user id and take data from backend
+};
+
+
 var addModalFor = true;
 var addModal = `
 <div class="modal fade" id="addExpIncModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -14,7 +21,7 @@ var addModal = `
         <form>
           <div class="form-group">
             <div class="dropdown">
-              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <button class="btn btn-secondary btn-dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Choose category
               </button>
               <select class="dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -27,11 +34,11 @@ var addModal = `
 
           <div class="form-group">
             <label for="amount-name" class="col-form-label">Amount:</label>
-            <input type="password" class="form-control" id="amount-name-input">
+            <input type="text" class="form-control" id="amount-name-input">
           </div>
 
           <div class="form-group">
-            <label for="expense-income-name" class="col-form-label">${addModalFor ? 'Expenses name:' : 'Income name:'}</label>
+            <label for="expense-income-name" class="col-form-label">${addModalFor ? 'Expense name:' : 'Income name:'}</label>
             <input type="text" class="form-control" id="expense-income-name-input">
           </div>
 
@@ -40,7 +47,7 @@ var addModal = `
 
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary close-footer" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary btn-action">Insert${addModalFor ? ' expense' : ' income'}</button>
+        <button type="button" class="btn btn-primary btn-action-add-exp-inc">Insert${addModalFor ? ' expense' : ' income'}</button>
       </div>
     </div>
   </div>
@@ -57,13 +64,51 @@ $("body").on("click", ".close, .close-footer", function () {
     $("#addExpIncModal").modal("hide");
 });
 
-$('.dropdown-toggle').click(function () {
+$('.btn-dropdown-toggle').click(function () {
     $('.dropdown-menu').toggle();
 });
 
-$("body").on("click", ".dropdown-menu a", function () {
-    var selectedCategory = $(this).text();
-    $("#dropdownMenuButton").text(selectedCategory);
+$("body").on("click", ".btn-action-add-exp-inc", function () {
+    addExpense();
+    $("#addExpIncModal").modal("hide");
 });
 
+// to get value from dropdown
+var selectedValueCategory;
+$('select').on('change', function() {
+    selectedValueCategory = $(this).val();
+    console.log(selectedValueCategory);
+  });
+
+function addExpense() {
+    // if for future use
+    // const id = utils.getCurrentUserId();
+    const id = 5;
+
+    var expenseName = $("#expense-income-name-input").val();
+    var amount = $("#amount-name-input").val();
+    var category = selectedValueCategory;
+
+    var expense = {
+        expenseName: expenseName,
+        amount: amount,
+        category: category
+    };
+
+    console.log("add modal expense/income: ", expense);
+
+    $.ajax({
+        url: "/add_expenses/" + id,
+        method: "POST",
+        data: JSON.stringify(expense),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            console.log("Success: ", response);
+        },
+        error: function (xhr, status, error) {
+            console.log("Error: ", error);
+        }
+    });
+}
 
